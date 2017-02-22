f1e8f6114a4e3c1bcd2e4509a14c99fce1506b20
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-02-22 18:48:51 +0900

    add column user model
    t.string :username  null: false
    t.text    :member
    t.text    :profile
    t.text    :works,

--- db/migrate/20170222092450_devise_create_users.rb
+++ db/migrate/20170222092450_devise_create_users.rb
@@ -2,6 +2,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :username,           null: false
+      t.text   :member
+      t.text   :profile
+      t.text   :works
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
