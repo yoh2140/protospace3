33eb3b30f4c6e36af1e34547fe5a63c564c88c03
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-02-22 13:15:00 +0900

    first commit

---
+++ config/application.rb
@@ -0,0 +1,27 @@
+require File.expand_path('../boot', __FILE__)
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Protospace3
+  class Application < Rails::Application
+    config.generators do |g|
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
