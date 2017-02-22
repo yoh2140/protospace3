14eac031b9c5383a71c04ea41a282acbda2b311d
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-02-22 19:25:24 +0900

    add association user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :prototypes
+  has_many :comments
+  has_many :likes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
