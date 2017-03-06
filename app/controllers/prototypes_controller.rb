ba6e44d5f72568bfdda065f7d22a4f016591bb1f
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-03-06 17:19:58 +0900

    add prototype action(create, destroy, edit, update) add set_prototype

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :set_prototype, only: [:edit, :destroy, :update]
 
   def index
   end
@@ -9,5 +10,21 @@ class PrototypesController < ApplicationController
 
   def show
   end
-  
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
 end
