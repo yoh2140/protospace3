7f9e7344ead3eb328b573b3b66f1bdff81df7d65
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-03-11 13:30:55 +0900

    add image table, content :text prototype(association) :references

--- db/migrate/20170311042401_create_images.rb
+++ db/migrate/20170311042401_create_images.rb
@@ -1,6 +1,8 @@
 class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
+      t.text       :content
+      t.references :prototype
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306075452) do
+ActiveRecord::Schema.define(version: 20170311042401) do
+
+  create_table "images", force: :cascade do |t|
+    t.text     "content",      limit: 65535
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "prototypes", force: :cascade do |t|
-    t.string   "title",      limit: 255
+    t.string   "title",      limit: 255,   default: ""
     t.text     "cach_copy",  limit: 65535
     t.text     "concept",    limit: 65535
     t.integer  "user_id",    limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
   end
 
   add_index "prototypes", ["user_id"], name: "fk_rails_8add56efc2", using: :btree
