6bf0b85e6eca8511918716267285ae7243a8f688
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-03-18 20:19:12 +0900

    update authenticate_user! to all action

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only:  %i(new edit destroy)
+  before_action :authenticate_user!
   before_action :set_prototype, only: %i(show edit destroy update)
 
   def index
