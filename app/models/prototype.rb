7996285f62c4174ebdde07366989f286a1bff120
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-03-22 20:41:53 +0900

    add find_by method to likes user.id

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -8,4 +8,9 @@ class Prototype < ActiveRecord::Base
   validates :title, :cach_copy, :concept, presence:true
 
   paginates_per 8
+
+  def like_user(user)
+    likes.find_by(user_id: user)
+  end
+
 end
