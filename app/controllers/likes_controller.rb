8f1773d7494f1ab8642d093f42f13603bc98cf1f
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-03-22 21:00:45 +0900

    add create action to likes_controller, Like.create user_id(current_user) and prototype_id(prototype.id), add delete action to likes_controller, destroy Like.id, after that prototype.id to @prototype

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
+
+  def create
+    @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @prototype = @like.prototype
+  end
+
+  def destroy
+    like = Like.find(params[:id])
+    like.destroy
+    @prototype = Prototype.find(params[:prototype_id])
+  end
+
 end
