d35651c1140e9e457eb88da3f0d12c35bc79170b
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-03-23 17:55:02 +0900

    add association prototype and user models, has_many :comments

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
   has_many :images, dependent: :destroy
 
   accepts_nested_attributes_for :images, reject_if: proc { |attributes| attributes['content'].blank? }, allow_destroy: true
