dcd3c3ca3b015b6b276ba881ab45773fc7052b28
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-03-25 18:08:35 +0900

    add create action, add  authenticate_user!

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,13 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    comment = current_user.comments.create(create_comment)
+    @comments = comment.prototype.comments
+  end
+
+  private
+  def create_comment
+    params.require(:comment).permit(:content).merge(prototype_id: params[:prototype_id])
   end
 end
