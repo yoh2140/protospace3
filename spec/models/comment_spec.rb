26fcecc502c7283ef14b8565dfd94c7a8fb9bedc
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-04-20 18:15:05 +0900

    update comment spec to association, combine let, 	let!(:prototype) { create(:prototype) }     	let!(:user) { create(:user) } 	let!(:comment) { create(:comment, prototype: prototype, user: user)}

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,35 +1,29 @@
 require 'rails_helper'
 
 describe Comment do
-  describe '#association' do
 
+  describe '#association' do
+    let!(:prototype) { create(:prototype) }
+    let!(:user) { create(:user) }
+    let!(:comment) { create(:comment, prototype: prototype, user: user)}
     it "Prototypeと関連づけられている" do
-      prototype = create(:prototype)
-      comment = create(:comment, prototype: prototype)
       expect(comment.prototype).to eq prototype
     end
-
     it "Userと関連づけられている" do
-      user = create(:user)
-      comment = create(:comment, user: user)
       expect(comment.user).to eq user
     end
-
   end
 
   describe '#validation' do
-
     it "正常に投稿できました。" do
       comment = build(:comment)
       expect(comment).to be_valid
     end
-
     it "コメント欄が空欄です。" do
       comment = build(:comment, content: nil)
       comment.valid?
       expect(comment.errors[:content]).to include("can't be blank")
     end
-
   end
 
 end
