bddf1842d04e6229374216e9e5be695c78901acc
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-04-21 20:22:39 +0900

    add index action test, context before login_user

--- spec/controllers/prototypes_controller_spec.rb
+++ spec/controllers/prototypes_controller_spec.rb
@@ -11,5 +11,17 @@ describe PrototypesController, type: :controller do
      prototype: attributes_for(:prototype, title: nil, tag_list: 'bbbbbb')
    }}
 
+  context 'ユーザーはログインされています。' do
+    before { login_user }
+
+    describe 'GET #index' do
+      before { get :index }
+      it "@prototypesは正常に割り当てられています。" do
+        expect(assigns(:prototypes)).to include prototype
+      end
+      it "indexテンプレートに遷移できました。" do
+        expect(response).to render_template :index
+      end
+    end
 
 end
