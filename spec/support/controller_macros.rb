7bfa554540eebf650217fa7d4253afda0424ff80
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-04-21 20:19:19 +0900

    create controller_macros filem

---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,7 @@
+module ControllerMacros
+  def login_user(user=nil)
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    user ||= FactoryGirl.create(:user)
+    sign_in user
+  end
+end
