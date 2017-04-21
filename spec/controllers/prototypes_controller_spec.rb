c3ec852518f300d1dd05e84dfd7e2ab55fede5eb
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-04-21 20:24:15 +0900

    add create action test

--- spec/controllers/prototypes_controller_spec.rb
+++ spec/controllers/prototypes_controller_spec.rb
@@ -34,4 +34,33 @@ describe PrototypesController, type: :controller do
       end
     end
 
+    describe 'POST#create' do
+      context '成功した場合' do
+        before { post :create, params }
+        it '新しい情報をデータベースに保存できました。' do
+          expect{ post :create, params }.to change(Prototype, :count).by(1)
+        end
+        it 'トップにリダイレクトしました。' do
+          expect(response).to redirect_to root_path
+        end
+        it '作成成功メッセージが表示されました。' do
+          expect(flash[:success]).to eq '作成できました。'
+        end
+      end
+      context '失敗した場合' do
+        before { post :create, invalid_params}
+        it '@prototypeに'
+        it '情報をデーターベースに保存できませんでした。' do
+          expect{ post :create, invalid_params }.not_to change(Prototype, :count)
+        end
+        it '記事作成ページにリダイレクトしました。' do
+          expect(response).to render_template :new
+        end
+        it '作成失敗メッセージが表示されました。' do
+          expect(flash[:danger]).to eq '作成に失敗しました。'
+        end
+      end
+      
+    end
+
 end
