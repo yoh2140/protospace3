84c5d6aa6c1cc5459bace754ff01db395e7cdbbf
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-04-24 19:26:24 +0900

    add let :user and :params   let(:user) { create(:user) }   let(:params) {{     id: user.id,     user: attributes_for(:user, name: 'aaaaaa')     }}

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,1 +1,10 @@
 require 'rails_helper'
+
+describe UsersController do
+  let(:user) { create(:user) }
+  let(:params) {{
+    id: user.id,
+    user: attributes_for(:user, name: 'aaaaaa')
+    }}
+
+end
