998de9726d49d3174b1265a8a769246a962b31f3
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-04-24 20:57:04 +0900

    add update action test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,7 +4,28 @@ describe UsersController do
   let(:user) { create(:user) }
   let(:params) {{
     id: user.id,
-    user: attributes_for(:user, name: 'aaaaaa')
+    user: attributes_for(:user, username: 'aaaaaa')
     }}
 
+  context 'ユーザーはログインされている。' do
+    before { login_user }
+
+    describe 'PATCH #update' do
+      before { patch :update, params }
+      it '@userは正常に割り当てられています。' do
+        expect(assigns(:user)).to eq user
+      end
+      it '更新が完了しました。' do
+        user.reload
+        expect(user.username).to eq 'aaaaaa'
+      end
+      it 'トップページにリダイレクトしました。' do
+        expect(response).to redirect_to root_path
+      end
+      it '更新成功メッセージが表示されました。' do
+        expect(flash[:notice]).to eq '変更が完了しました。'
+      end
+    end
+
+  end
 end
