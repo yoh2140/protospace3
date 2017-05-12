f0dfc528287e0a2037805859f7b761ed24e840d4
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-05-12 21:28:45 +0900

    Non ajax, add expect to registered with user

--- spec/features/user_test.rb
+++ spec/features/user_test.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature 'Sign in', type: :feature do
   given(:user) { build(:user) }
 
-  scenario '新規ユーザー作成', js: true do
+  scenario '新規ユーザー作成' do
     visit root_path
     click_on '新規登録'
     fill_in 'Username', with: user.username
@@ -12,7 +12,7 @@ RSpec.feature 'Sign in', type: :feature do
     fill_in 'Member', with: user.member
     fill_in 'Profile', with: user.profile
     fill_in 'Works', with: user.works
-    click_on '作成する'
+    expect { click_on '作成する' }.to change(User, :count).by(1)
     expect(page).to have_selector '.alert-notice', text: 'Welcome! You have signed up successfully.'
     end
   end
