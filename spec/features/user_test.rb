c4d63e033a5526d51c0039b2d2d35d07ca7d2ace
yoh <koh@kouchiyouhei-no-MacBook-Air.local>
2017-05-12 18:01:07 +0900

    create feature test user sign_up

---
+++ spec/features/user_test.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature 'Sign in', type: :feature do
+  given(:user) { build(:user) }
+
+  scenario '新規ユーザー作成', js: true do
+    visit root_path
+    click_on '新規登録'
+    fill_in 'Username', with: user.username
+    fill_in 'E-mail', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Member', with: user.member
+    fill_in 'Profile', with: user.profile
+    fill_in 'Works', with: user.works
+    click_on '作成する'
+    expect(page).to have_selector '.alert-notice', text: 'Welcome! You have signed up successfully.'
+    end
+  end
